e8ec4c8cc95814895c61214a2f0e753fbafa6cad
Dylan <dylandude206@gmail.com>
2026-07-28 11:36:46 -0400

    Current testing opmode, updated motor name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOADCode/dylan_code/Dylan_current_testing_pro_board.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOADCode/dylan_code/Dylan_current_testing_pro_board.java
@@ -9,10 +9,10 @@ public class Dylan_current_testing_pro_board {
     private double ticksPerRotation;
 
 public void init (HardwareMap hwMap) {
-    motor = hwMap.get(DcMotorEx.class, "Intake");
-motor=hwMap.get(DcMotorEx.class,"Intake");
+    motor = hwMap.get(DcMotorEx.class, "intake");
+motor=hwMap.get(DcMotorEx.class,"intake");
 // "intake" is just pod motor //
-motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 ticksPerRotation=motor.getMotorType().getTicksPerRev();
 
 }
