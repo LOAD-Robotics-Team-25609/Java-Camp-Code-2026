12af8f828c1152f6a860a7f4535c9da80b7f5d13
abhi <abhi051214@gmail.com>
2026-07-28 12:19:14 -0400

    changes made by Abhi the sonion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOADCode/abhi_the_sonion_code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOADCode/abhi_the_sonion_code.java
@@ -98,7 +98,12 @@ public class abhi_the_sonion_code extends OpMode
 
         );
         if (gamepad1.right_trigger>0.1) {
-            intake.setPower((-gamepad1.right_trigger));
+            intake.setPower(-gamepad1.right_trigger);
+        }else{
+            intake.setPower(0);
+        }
+        if (gamepad1.left_trigger>0.1) {
+            intake.setPower(gamepad1.left_trigger);
         }else{
             intake.setPower(0);
         }
