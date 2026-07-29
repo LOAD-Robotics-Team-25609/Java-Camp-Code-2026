4273ba15e1fd0495f51f9725c6dd6ef4acdcc67f
abhi <abhi051214@gmail.com>
2026-07-29 09:48:45 -0400

    changes made by Abhi the sonion

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOADCode/abhi_code/abhi_the_sonion_auto.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.LOADCode.abhi_code;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import static dev.nextftc.extensions.pedro.PedroComponent.follower;
+
+import com.pedropathing.geometry.Pose;
+
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+
+public class abhi_the_sonion_auto extends NextFTCOpMode{
+    {
+        addComponents(
+                new PedroComponent(Constants::createFollower)
+        );
+    }
+
+    @Override public void onInit() {
+        follower().setStartingPose(new Pose(88, 7.1, Math.toRadians(90)));
+    }
+    @Override public void onWaitForStart() { }
+    @Override public void onStartButtonPressed() { }
+    @Override public void onUpdate() { }
+    @Override public void onStop() { }
+}
