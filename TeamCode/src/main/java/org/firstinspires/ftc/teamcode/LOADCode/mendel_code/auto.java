21bd31f5e3e6e46f02a68e97654d0c5fe66852c1
heal <ericnndzana25609@gmail.com>
2026-07-29 10:54:17 -0400

    spin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOADCode/mendel_code/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOADCode/mendel_code/auto.java
@@ -1,12 +1,18 @@
 package org.firstinspires.ftc.teamcode.LOADCode.mendel_code;
 
+import org.firstinspires.ftc.teamcode.Devices;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import static dev.nextftc.extensions.pedro.PedroComponent.follower;
 
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.core.commands.utility.LambdaCommand;
+import dev.nextftc.extensions.pedro.FollowPath;
 import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
 
@@ -18,16 +24,34 @@ public class auto extends NextFTCOpMode {
         );
     }
 
+    Devices.DcMotorExClass intake = new Devices.DcMotorExClass();
+
+Command setIntakeMode(boolean on){
+        return new InstantCommand(new LambdaCommand("setIntake")
+                .setStart(() -> {
+                    if (on) {
+                        intake.setPower(1);
+                    }else{
+                        intake.setPower(0);
+                    }
+                })
+        );
+}
+
     guardedfoil guardedfoil;
 
     @Override public void onInit() {
         follower().setStartingPose(new Pose(88,7.1, Math.toRadians(90)));
         guardedfoil = new guardedfoil(follower());
+        intake.init(this,"intake");
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
     }
     @Override public void onWaitForStart() { }
     @Override public void onStartButtonPressed() {
         new SequentialGroup(
-
+                setIntakeMode(true)        ,
+                new FollowPath(guardedfoil.MainChain),
+                        setIntakeMode(false)
         ).schedule();
     }
     @Override public void onUpdate() { }
