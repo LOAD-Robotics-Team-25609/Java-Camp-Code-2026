e460aa9cf72504bf14318c1721c39a17bf1dcdf3
BoomKitty08 <julietBehrens08@Gmail.com>
2026-08-21 17:25:15 -0400

    i got rid of my code from FTC summer camp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOADCode/juliet_code/Juliet_Code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOADCode/juliet_code/Juliet_Code.java
@@ -50,7 +50,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
-
+//yyy
 
 
 
